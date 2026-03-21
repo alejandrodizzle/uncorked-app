df5bab0873be09d7e399e0ec34a195a84b1f5eb0
alejandrodizzle <aarenas1@gmail.com>
2026-03-21 09:52:12 -0700

    feat: add ios/ Capacitor platform scaffold

---
+++ artifacts/uncorked/ios/App/App/public/assets/index-HgxxiOpt.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-JeJPV_ZB.js","assets/index-DxpbdEtj.js"])))=>i.map(i=>d[i]);
+import{_ as r}from"./index-bk8b0zQk.js";import{r as o}from"./index-DxpbdEtj.js";const i=o("SplashScreen",{web:()=>r(()=>import("./web-JeJPV_ZB.js"),__vite__mapDeps([0,1])).then(e=>new e.SplashScreenWeb)});export{i as SplashScreen};
